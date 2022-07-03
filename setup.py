cf0774ff85cd9e346a090749fdae5033e8c9db30
unknown <dan_kavoulakos@hotmail.com>
2022-07-03 11:10:16 +0300

    setup url added

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ setup(
 	author='Daniel Kavoulakos',
 	author_email='dan_kavoulakos@hotmail.com',
 	description='Create Ableton Live 11 Drum Rack presets.',
+	url='https://github.com/danikavu/Ableton-Drum-Rack',
 	license='MIT',
 	packages=['.abletondrumrack'],
-    package_data={'.abletondrumrack': ['ableton_files/*']},
-	install_requires=['numpy',
-					  'pandas',
-                      'SoundFile',
-					  ],
+	package_data={'.abletondrumrack': ['ableton_files/*']},
+	install_requires=[
+		'numpy',
+		'pandas',
+		'SoundFile',
+		],
 	python_requires='>=3.7',
 )
-					  
